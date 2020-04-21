63d5cbd9565da85a38d8a31156d2bd13148a2103
Jim McGuinness <dador92@dador-iMac-2019.local>
2020-04-21 11:34:07 -0500

    added script methods to download and unzip the data set

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,10 @@
 ## run_analysis.R
+
+# 0. set up the environment ----------
+
+
+# 1. download, unzip, load the source data sets ------------
+
+sourceUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url=sourceUrl, destfile="./Dataset.zip", method="curl") 
+unzip("Dataset.zip", exdir = "./")
